8bb24844d8373384412ae10f7b77c8c47d928653
Jiniyas Shah <jiniyasshahts@gmail.com>
2026-01-08 09:48:29 +0545

    refactor: restructure logger to utilize core.AttackLog and improve subscriber management

--- gateway/internal/utils/logger/logger.go
+++ gateway/internal/utils/logger/logger.go
@@ -1,24 +1,55 @@
 package logger
 
 import (
-	"web-app-firewall-ml-detection/internal/detector" // Imported and USED now
-
-	"go.mongodb.org/mongo-driver/mongo"
+	"sync"
+	"web-app-firewall-ml-detection/internal/core"
 )
 
-// [DELETED] FullRequest and AttackLog structs are removed from here.
-// They are now imported from "web-app-firewall-ml-detection/internal/detector"
-
-var logCollection *mongo.Collection
-
-// [UPDATED] Use detector.AttackLog
-var broadcast = make(chan detector.AttackLog, 100)
-
-func Init(client *mongo.Client, dbName string) {
-	logCollection = client.Database(dbName).Collection("logs")
+// Broker manages the list of active SSE clients
+type Broker struct {
+	mu          sync.RWMutex // Read/Write lock for thread safety
+	subscribers map[chan core.AttackLog]struct{}
 }
 
-// [UPDATED] Use detector.AttackLog
-func GetBroadcastChannel() chan detector.AttackLog {
-	return broadcast
+// Global broker instance
+var broker = &Broker{
+	subscribers: make(map[chan core.AttackLog]struct{}),
 }
+
+// creates a new dedicated channel for a client
+func Subscribe() chan core.AttackLog {
+	broker.mu.Lock()
+	defer broker.mu.Unlock()
+
+	// Buffer of 50 prevents minor network lag from dropping logs
+	ch := make(chan core.AttackLog, 50)
+	broker.subscribers[ch] = struct{}{}
+	return ch
+}
+
+//  removes a client and closes their channel
+func Unsubscribe(ch chan core.AttackLog) {
+	broker.mu.Lock()
+	defer broker.mu.Unlock()
+
+	if _, ok := broker.subscribers[ch]; ok {
+		delete(broker.subscribers, ch)
+		close(ch) 
+	}
+}
+
+// broadcasts a log entry to ALL active subscribers
+func LogAttack(entry core.AttackLog) {
+	broker.mu.RLock() // concurrent broadcasting
+	defer broker.mu.RUnlock()
+
+	for ch := range broker.subscribers {
+		select {
+		case ch <- entry:
+			// Message sent successfully
+		default:
+			// Drop message for slower clients to protect the WAF from blocking.
+		}
+	}
+}
+
